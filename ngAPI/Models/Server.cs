3022463d4495b5df74d34f1554fff5082283dea4
Caleb.Rohdy <rohdy.cale@gmail.com>
2019-06-10 15:30:36 -0400

    add models to api

---
+++ ngAPI/Models/Server.cs
@@ -0,0 +1,9 @@
+namespace ngAPI.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Online { get; set; }
+    }
+}
