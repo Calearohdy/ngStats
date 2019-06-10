8b11ff7a0b0772f3194cb77f5f88f1e421cae0d0
Caleb.Rohdy <rohdy.cale@gmail.com>
2019-06-09 20:52:10 -0400

    add piechart

--- src/app/charts/piechart/piechart.component.ts
+++ src/app/charts/piechart/piechart.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PiechartComponent implements OnInit {
 
+  public pieChartData: number[] = [350, 150, 220];
+  public pieChartLabels: string[] = ['cust1', 'cust2', 'cust3'];
+  public pieChartType = 'pie';
+  public colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
